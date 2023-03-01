7ea12465aceb3d1dbef08e727da5747cdd3b52da
Grzesiek <g.durtan@temida.io>
2023-03-01 09:32:51 +0100

    test imports

--- index.ts
+++ index.ts
@@ -1,11 +1,7 @@
-import { Block } from "./Block";
-import { Color } from "./Color";
-import { Grid } from "./Grid";
-import { Hr } from "./Hr";
-import { HStack } from "./HStack";
-import { Space } from "./Space";
-import { VStack } from "./VStack";
+export { Block } from "./Block";
+export { Color } from "./Color";
+export { Hr } from "./Hr";
+export { HStack } from "./HStack";
+export { Space } from "./Space";
+export { VStack } from "./VStack";
 
-
-
-export { Block, Color, Grid, Hr, HStack, Space, VStack}
